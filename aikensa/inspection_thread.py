18e4a056d20e603ee8724c475cca292cbe2b0aed
stefanoKS <stefano.ifan@gmail.com>
2025-04-22 15:36:27 +0900

    minor bug fixes

--- aikensa/inspection_thread.py
+++ aikensa/inspection_thread.py
@@ -592,8 +592,8 @@ class InspectionThread(QThread):
                             self.save_image_result(self.combinedImage, self.InspectionImages[0], self.InspectionResult_Status[0])
 
                             self.save_result_database(partname = self.widget_dir_map[self.inspection_config.widget],
-                                    numofPart = self.inspection_config.current_numofPart[self.inspection_config.widget],
-                                    currentnumofPart = self.inspection_config.today_numofPart[self.inspection_config.widget], 
+                                    numofPart = self.inspection_config.today_numofPart[self.inspection_config.widget], 
+                                    currentnumofPart = self.inspection_config.current_numofPart[self.inspection_config.widget], 
                                     deltaTime = 0.0,
                                     kensainName = self.inspection_config.kensainNumber, 
                                     detected_pitch_str = self.InspectionResult_PitchMeasured[0], 
